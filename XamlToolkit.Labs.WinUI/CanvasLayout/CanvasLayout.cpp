891acd74776ae24fe4cbd26d47984573983364b0
lgztx <z2337878191@outlook.com>
2025-11-06 00:23:24 +0800

    Add CanvasLayout

---
+++ XamlToolkit.Labs.WinUI/CanvasLayout/CanvasLayout.cpp
@@ -0,0 +1,86 @@
+﻿#include "pch.h"
+#include "CanvasLayout.h"
+#if __has_include("CanvasLayout.g.cpp")
+#include "CanvasLayout.g.cpp"
+#endif
+
+namespace winrt::XamlToolkit::Labs::WinUI::implementation
+{
+	void CanvasLayout::InitializeForContextCore(VirtualizingLayoutContext const& context)
+	{
+		base_type::InitializeForContextCore(context);
+
+		if (!context.LayoutState().try_as<ICanvasLayoutState>())
+		{
+			// Store any state we might need since (in theory) the layout could be in use by multiple
+			// elements simultaneously
+			context.LayoutState(winrt::make<CanvasLayoutState>());
+		}
+	}
+
+	void CanvasLayout::UninitializeForContextCore(VirtualizingLayoutContext const& context)
+	{
+		base_type::UninitializeForContextCore(context);
+
+		// clear any state
+		context.LayoutState(nullptr);
+	}
+
+	Size CanvasLayout::MeasureOverride(VirtualizingLayoutContext const& context, Size availableSize)
+	{
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        // Get underlying data about positioning of items and determine if in viewport.
+		auto realizationRect = context.RealizationRect();
+        for (int i = 0; i < context.ItemCount(); i++)
+        {
+            if (auto item = context.GetItemAt(i).try_as<ICanvasLayoutItem>())
+            {
+                // See if this item pushes our maximum boundary
+                maxWidth = std::max<int>(item.Left() + item.Width(), maxWidth);
+                maxHeight = std::max<int>(item.Top() + item.Height(), maxHeight);
+
+                // Calculate if this item is in our current viewport
+                Rect rect(item.Left(), item.Top(), item.Width(), item.Height());
+                RectHelper::Intersect(rect, realizationRect);
+
+                // Check if we're in view now so we can compare to if we were last time.
+                bool nowInView = rect.Width > 0 || rect.Height > 0;
+
+                // If it wasn't visible and now is, realize the container
+                if (nowInView && !item.IsInView())
+                {
+                    auto element = context.GetOrCreateElementAt(i);
+                    element.Measure(Size(item.Width(), item.Height()));
+                }
+                // If it was visible, but now isn't recycle the container
+                else if (!nowInView && item.IsInView())
+                {
+                    auto element = context.GetOrCreateElementAt(i);
+                    context.RecycleElement(element);
+                }
+
+                // Update our current visibility
+                item.IsInView(rect.Width > 0 || rect.Height > 0);
+            }
+        }
+
+        return Size(maxWidth, maxHeight);
+	}
+
+	Size CanvasLayout::ArrangeOverride(VirtualizingLayoutContext const& context, Size finalSize)
+	{
+        for (int i = 0; i < context.ItemCount(); i++)
+        {
+            if (auto item = context.GetItemAt(i).try_as<ICanvasLayoutItem>(); item && item.IsInView())
+            {
+                auto container = context.GetOrCreateElementAt(i);
+                // Is it better to have cached this from above?
+                container.Arrange(Rect(item.Left(), item.Top(), item.Width(), item.Height()));
+            }
+        }
+
+        return finalSize;
+	}
+}
---
+++ XamlToolkit.Labs.WinUI/CanvasLayout/CanvasLayout.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CanvasLayout.g.h"
+
+namespace winrt::XamlToolkit::Labs::WinUI::implementation
+{
+	struct CanvasLayoutState : winrt::implements<CanvasLayoutState, ICanvasLayoutState>
+	{
+	public:
+		CanvasLayoutState() : FirstRealizedIndex{ -1 }, LayoutRects{ winrt::single_threaded_vector<Rect>() } { }
+
+		wil::single_threaded_rw_property<int> FirstRealizedIndex;
+		wil::single_threaded_rw_property<IVector<Rect>> LayoutRects;
+	};
+
+	struct CanvasLayout : CanvasLayoutT<CanvasLayout>
+	{
+		CanvasLayout() = default;
+
+#pragma region Setup / teardown
+		void InitializeForContextCore(VirtualizingLayoutContext const& context);
+
+		void UninitializeForContextCore(VirtualizingLayoutContext const& context);
+#pragma endregion
+
+#pragma region Layout
+		Size MeasureOverride(VirtualizingLayoutContext const& context, Size availableSize);
+
+		Size ArrangeOverride(VirtualizingLayoutContext const& context, Size finalSize);
+#pragma endregion
+	};
+}
+
+namespace winrt::XamlToolkit::Labs::WinUI::factory_implementation
+{
+	struct CanvasLayout : CanvasLayoutT<CanvasLayout, implementation::CanvasLayout>
+	{
+	};
+}
